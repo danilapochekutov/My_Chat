164bd3acf2022c285bbad26994fdebcf21779d2b
danilapochekutov <danya270203@gamil.com>
2024-08-26 18:38:59 +0300

    added gh-pages build

--- src/utils/consts.ts
+++
@@ -1,3 +0,0 @@
-export const LOGIN_ROUTE = "/login";
-export const CHAT_ROUTE = "/chat";
-export const REGISTER_ROUTE = "/register";
--- src/utils/variables.ts
+++
@@ -1,5 +0,0 @@
-import { deepPurple } from "@mui/material/colors";
-import { blue } from "@mui/material/colors";
-export const color = deepPurple["A400"];
-
-export const colorChat = blue["A400"];
